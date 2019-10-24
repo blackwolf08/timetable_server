5a9098b12ea6646b4647381d3f4214d1d8236b81
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-10-25 03:03:54 +0530

    Update __init__.py

--- webkiosk/__init__.py
+++ webkiosk/__init__.py
@@ -4,9 +4,6 @@ import os
 import xlrd 
 
 
-
-
-
 def getTimeTable(batch,year,college,subs):
     currDir = os.path.dirname(__file__)
     if year == "1":
@@ -19,6 +16,16 @@ def getTimeTable(batch,year,college,subs):
         loc = (os.path.join(currDir,"B.TECH VII Sem.xlsx"))
     else:
         loc = (os.path.join(currDir,"M TECH I Sem, DD.xlsx"))
+#     if year == "1":
+#         loc = "B Tech I Sem.xlsx"
+#     elif year == "2":
+#         loc = "B Tech III Sem.xlsx"
+#     elif year == "3":
+#         loc = "B Tech V Sem.xlsx"
+#     elif year == "4":
+#         loc = "B.TECH VII Sem.xlsx"
+#     else:
+#         loc ="M TECH I Sem, DD.xlsx"
 
 
     wb = xlrd.open_workbook(loc) 
@@ -28,6 +35,14 @@ def getTimeTable(batch,year,college,subs):
 
     for idx, sub in enumerate(subs):
         subs[idx] = sub[len(sub)-4:]
+        if year == "1":
+            range_year = 102
+        if year == "2":
+            range_year = 107
+        if year == "3":
+            range_year = 85
+        if year == "4":
+            range_year = 70
 
 
     key = {}
@@ -55,33 +70,45 @@ def getTimeTable(batch,year,college,subs):
     for i in range(sheet.ncols): 
         if sheet.cell_value(1,i) != "":
             key['SAT'][sheet.cell_value(1,i)] = ""
-    if year == "3" or year == "1":
-        del key['MON']['12 NOON-12.50 PM']
-        del key['TUES']['12 NOON-12.50 PM']
-        del key['WED']['12 NOON-12.50 PM']
-        del key['THUR']['12 NOON-12.50 PM']
-        del key['FRI']['12 NOON-12.50 PM']
-        del key['SAT']['12 NOON-12.50 PM']
+    try:
+        if year == "3" or year == "1":
+            del key['MON']['12 NOON-12.50 PM']
+            del key['TUES']['12 NOON-12.50 PM']
+            del key['WED']['12 NOON-12.50 PM']
+            del key['THUR']['12 NOON-12.50 PM']
+            del key['FRI']['12 NOON-12.50 PM']
+            del key['SAT']['12 NOON-12.50 PM']
 
-    if year == "2" or year == "4":
-        del key['MON']['1- 1.50 PM']
-        del key['TUES']['1- 1.50 PM']
-        del key['WED']['1- 1.50 PM']
-        del key['THUR']['1- 1.50 PM']
-        del key['FRI']['1- 1.50 PM']
-        del key['SAT']['1- 1.50 PM']
-
+        if year == "2" or year == "4":
+            del key['MON']['1- 1.50 PM']
+            del key['TUES']['1- 1.50 PM']
+            del key['WED']['1- 1.50 PM']
+            del key['THUR']['1- 1.50 PM']
+            del key['FRI']['1- 1.50 PM']
+            del key['SAT']['1- 1.50 PM']
+    except:
+        print("--del key err")
+        pass
+    batch = batch.replace(" ","")
+    batch = batch.replace("%20","")
+    batch = batch.replace("-","")
+    batch = batch.upper()
     for sub in subs:
         for i in range(sheet.ncols): 
-            for j in range(85):
-                if sheet.cell_value(j, i).find(sub) != -1 and (sheet.cell_value(j, i).find(f'-{batch[1:]}') != -1 or sheet.cell_value(j, i).find(batch) != -1 or sheet.cell_value(j, i).find("ABC") != -1 or sheet.cell_value(j, i).find(f',{batch[1:]},') != -1 or sheet.cell_value(j, i).find(f'{batch[1:]}-') != -1) and sheet.cell_value(j, i).find(f'{batch[0:1]}') != -1:
+            for j in range(range_year):
+                if sheet.cell_value(j, i).find(sub) != -1 and (sheet.cell_value(j, i).split("(")[0].find(f'-{batch[1:]}') != -1 or sheet.cell_value(j, i).split("(")[0].find(batch) != -1 or sheet.cell_value(j, i).split("(")[0].find("ABC") != -1 or sheet.cell_value(j, i).split("(")[0].find(f',{batch[1:]},') != -1 or sheet.cell_value(j, i).split("(")[0].find(f'{batch[1:]}-') != -1) and sheet.cell_value(j, i).split("(")[0].find(f'{batch[0:1]}') != -1:
                     row = j
                     while(row>0):
                         if sheet.cell_value(row,0) == "MON" or sheet.cell_value(row,0) == "TUES" or sheet.cell_value(row,0) == "WED" or sheet.cell_value(row,0) == "THUR" or sheet.cell_value(row,0) == "FRI" or sheet.cell_value(row,0) == "SAT":
                             day = sheet.cell_value(row,0)
                             break
                         row-=1
-                    key[day][sheet.cell_value(1,i)] = sheet.cell_value(j, i)
+                    if key[day][sheet.cell_value(1,i)] != "" and sheet.cell_value(j, i).split("(")[0].find(batch) != -1:
+                        key[day][sheet.cell_value(1,i)] = sheet.cell_value(j, i)
+                    else:
+                        key[day][sheet.cell_value(1,i)] = sheet.cell_value(j, i)
+
+                    
     res = key
     json_data = {}
     to_send = {}
@@ -134,7 +161,7 @@ def getTimeTable(batch,year,college,subs):
                 elif res[key][child_key][0:1] == "L":
                     class_type = "Lecture"
                 elif res[key][child_key][0:1] == "T":
-                    class_type = "Practical"
+                    class_type = "Tutorial"
                 teachers = ""
                 for currIndex in range(len(res[key][child_key])-1,0,-1):
                     if(res[key][child_key][currIndex]=="/"):
@@ -160,6 +187,7 @@ def getTimeTable(batch,year,college,subs):
                 json_data[json_data_key].append([time,class_type,"",code,venue,teachers])
                 
     return json_data
+
 
 app = Flask(__name__) 
 
